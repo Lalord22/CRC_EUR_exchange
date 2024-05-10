0a2bacdbf3898fdf5b52a05517c95f38a15dc47c
Lalord22 <lalo22296@hotmail.com>
2024-05-09 18:19:14 -0600

    Converts crc to eur

--- app/src/main/java/com/example/crc_eur_exchange/MainActivity.kt
+++ app/src/main/java/com/example/crc_eur_exchange/MainActivity.kt
@@ -19,6 +19,9 @@ class MainActivity : AppCompatActivity() {
         val etName = findViewById<AppCompatEditText>(R.id.etName)
         val etNameCR = findViewById<AppCompatEditText>(R.id.etNameCR)
 
+        // Flag to indicate whether text change in etNameCR is caused by user input
+        var isUserInputOnEtNameCR = true
+
         etName.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
                 // Not needed
@@ -29,18 +32,47 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun afterTextChanged(s: Editable?) {
-                // Calculate the converted amount and set it in etNameCR
-                val inputText = s.toString()
-                if (inputText.isNotEmpty()) {
-                    val amountInEuros = inputText.toDouble()
-                    val convertedAmountInColones = amountInEuros * conversionRate
-                    etNameCR.setText(convertedAmountInColones.toString())
+                if (isUserInputOnEtNameCR) {
+                    val inputText = s.toString()
+                    if (inputText.isNotEmpty()) {
+                        val amountInEuros = inputText.toDouble()
+                        val convertedAmountInColones = amountInEuros * conversionRate
+                        isUserInputOnEtNameCR = false
+                        etNameCR.setText(convertedAmountInColones.toString())
+                    } else {
+                        etNameCR.text = null
+                    }
                 } else {
-                    // Clear etNameCR if etName is empty
-                    etNameCR.text = null
+                    isUserInputOnEtNameCR = true
                 }
             }
         })
 
+        etNameCR.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // Not needed
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // Not needed
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if (isUserInputOnEtNameCR) {
+                    val inputText = s.toString()
+                    if (inputText.isNotEmpty()) {
+                        val amountInColones = inputText.toDouble()
+                        val convertedAmountInEuros = amountInColones / conversionRate
+                        isUserInputOnEtNameCR = false
+                        etName.setText(convertedAmountInEuros.toString())
+                    } else {
+                        etName.text = null
+                    }
+                } else {
+                    isUserInputOnEtNameCR = true
+                }
+            }
+        })
     }
+
 }
